18c291d0049496fc25c108ecf510290933cadda5
zemichaell <zemichael2014@gmail.com>
2022-04-07 16:45:17 -0700

    Write a C program that prints the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,4 +11,4 @@ printf("size of an int: %d byte(s)\n", sizeof(int));
 printf("size of a long int: %d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
 print("size of a float: %d byte(s)\n", sizeof(float));
-return (0)
+return (0);
