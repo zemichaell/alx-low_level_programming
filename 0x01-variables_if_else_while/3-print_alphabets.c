6c51e7269690a0eabfead6d515c97f3308cfcc85
zemichaell <zemichael2014@gmail.com>
2022-04-08 13:59:13 -0700

    program that prints the alphabet in lowercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,14 +9,13 @@ int main(void)
 char upper_case_latter;
 char lower_case_latter;
 
-for (upper_case_latter = 'A'; upper_case_latter <= 'Z'; upper_case_latter ++)
-{
-putchar(upper_case_latter);
-}
-
-for (lower_case_latter ='a'; lower_case_latter <= 'z'; lower_case_latter ++)
+for (lower_case_latter = 'a'; lower_case_latter <= 'z'; lower_case_latter++)
 {
 putchar(lower_case_latter);
+}
+for (upper_case_latter = 'A'; upper_case_latter <= 'Z'; upper_case_latter)
+{
+putchar(upper_case_latter);
 }
 putchar('\n');
 
