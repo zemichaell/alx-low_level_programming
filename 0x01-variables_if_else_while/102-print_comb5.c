1f1f19077767f0ec954a2881290d46a5c864ae4f
zemichaell <zemichael2014@gmail.com>
2022-04-11 03:51:36 -0700

    Write a program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,8 +12,8 @@ while (a <= 99)
 b = a + 1;
 while (b <= 99)
 {
-putchar(a / 10) + num1;
-putchar(a % 10) + num1;
+putchar((a / 10) + num1);
+putchar((a % 10) + num1);
 putchar(32);
 putchar((b / 10) + num1);
 putchar((a % 10) + num1);
