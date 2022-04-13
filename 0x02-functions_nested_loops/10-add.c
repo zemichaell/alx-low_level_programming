1b9cac13ad7f4e8e7ab16950f3309aacf15aba9c
zemichaell <zemichael2014@gmail.com>
2022-04-13 07:24:49 -0700

    adding two integer numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 /**
  * add_two_numbers - add two integer number
  * @x: any enteger number
- * @y: any enteger number
+ * @y: any enteger numbre
+ * add: opretor which add two numbers
  * Return: Always 0.
  */
 int add(int x, int y)
