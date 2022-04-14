ffb219c3c493054de4a620079b3a30b3b0c227f0
zemichaell <zemichael2014@gmail.com>
2022-04-14 03:38:44 -0700

    program that computes and prints the sum of all the multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	int multiple = 0;
-	int sum = 0;
+int multiple = 0;
+int sum = 0;
 
-	while(multiple < 1024)
-	{
-		if (multiple % 3 == 0 || multiple % 5 == 0)
-		{
-			sum =+ multiple;
-		}
-		multiple += 1;
-	}
-	printf("%d\n", sum);
-	return (0);
+while(multiple < 1024)
+{
+if (multiple % 3 == 0 || multiple % 5 == 0)
+{
+sum =+ multiple;
+}
+multiple += 1;
+}
+printf("%d\n", sum);
+return (0);
 }
