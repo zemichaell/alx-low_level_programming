3b67546590d4dccc3ff6a31b23d71e50f32cd67c
zemichaell <zemichael2014@gmail.com>
2022-04-15 03:24:14 -0700

    Loop avoid

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - cause an infinite loop
+ * Return: 0
+ */
+in main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+
+/*
+ * whie (i < 10)
+ * {
+ * Infinite loop - No increse of variable
+ * putchar (i);
+}
+*/
+printf("Infinite loop avoid! \\o/\n");
+return (0);
+}
