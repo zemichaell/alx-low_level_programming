1e944a6a98ccc0da1740254ac7edab69bee3138f
zemichaell <zemichael2014@gmail.com>
2022-04-15 03:47:42 -0700

    Infinite loop avoided

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@ int main(void)
 {
 int i;
 
-printf("infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 
@@ -19,9 +19,6 @@ i = 0;
 *putchar(i);
 *}
 */
-
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
-
 }
