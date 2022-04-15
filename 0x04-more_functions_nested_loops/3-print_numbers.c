b5aa67a12f2ba1f2ffdb98c3e61db6468ae775dc
zemichaell <zemichael2014@gmail.com>
2022-04-15 14:50:12 -0700

    Function that prints numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints 0-9.
+ * 
+ * Return: number 0-9.
+ */
+
+void print_number(void)
+{
+int i;
+for (i = 0, i <= 9, i++)
+_putchar(i + '0');
+_putchar('\n');
+}
