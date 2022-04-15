9c6fe30da2720c9ffd59b8e11dc867a4dceeb9d8
zemichaell <71129973+zemichaell@users.noreply.github.com>
2022-04-16 02:20:13 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,28 +1,27 @@
 #include "main.h"
+
 /**
- * print_square - print the squer using # character 
+ * print_square - print the squer using # character
  * @size: size of the squer
  * Return: Always 0.
  */
 void print_square(int size)
 {
-int x;
-int y;
+	int x, y;
 
-if (size > 0)
-{
-for (x = 1; x <= size; x++)
-{
-for (y = 1; y <= size; y++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
-}
-else
-{
-_putchar('\n');
-}
-
+	if (size > 0)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = 1; y <= size; y++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
