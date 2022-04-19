341c85dd285c555e5a8910ed365b2204c7faee6a
zemichaell <zemichael2014@gmail.com>
2022-04-18 23:19:36 -0700

    Write a function that copies the string pointed to by src, including the terminating null byte

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: pointer to a char that will be changed
+ * @scr: pointer to a char that will be changed
+ *
+ * Return Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+i = 0;
+
+while (src[i]) != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+
+return (dest);
+
+}
