ba30d47e6917a6b3bd773057727c1b28a7a8acfe
zemichaell <zemichael2014@gmail.com>
2022-04-25 04:35:03 -0700

    Write a function that fills memory with a constant byte.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * _memset - the function that fills fist n bytes of memory area pointed to by @s
  * @s: memory area pointed by @s.
@@ -7,7 +9,6 @@
  *
  * Return: a pointer to the memory area @s.
  */
-
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int x;
