eba79064034cce141d2952d928a80c2c9c132252
zemichaell <71129973+zemichaell@users.noreply.github.com>
2022-05-26 06:31:51 -0700

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
- * get_bit - Target the bit of a number at the index position
- * @n: Given number
- * @index: Given index
- * Return: Positive number if success (0,1) or -1 if it fails
- */
+ * get_bit - Target the bit of a number at the index position.
+ * @n: Given number.
+ * @index: Given index.
+ * Return: Positive number if success (0,1) or -1 if it fails.
+ **/
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
